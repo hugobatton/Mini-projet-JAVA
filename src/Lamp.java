5518317c7d3fd17d7ddce463caa6aa7e3d677f46
battonh <battonh@PC-B103-08.iut-valence.fr>
2016-03-08 12:05:42 +0100

    Added some comments on the Lamp class

--- src/Lamp.java
+++ src/Lamp.java
@@ -7,10 +7,33 @@
 
 public class Lamp {
 	
-	private int x;
+	/**
+	 * first detail of lamp on the grid 
+	 */
+	
+	
+	private int x; 
+
+	/**
+	 * second detail of lamp on the grid 
+	 */
+	
 	private int y;
+	
+
+	/**
+	 * The following boolean is true when the lamp is on and false when the lamp is off
+	 */
+	
 	private boolean ESTALLUME;
 	
+	
+	/**
+	 * This constructor permit to create a lamp with its details and its state
+	 * @param x
+	 * @param y
+	 * @param ESTALLUME
+	 */
 	public Lamp (int x , int y, boolean ESTALLUME )
 	
 	{
