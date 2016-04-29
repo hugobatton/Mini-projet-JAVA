60473bdb21bc6c199f1f8afa1890f759d61c9b41
battonh <battonh@PC-B103-08.iut-valence.fr>
2016-04-29 14:35:22 +0200

    - Deleting of the class Lamp because we don't need it - Filling of the method switchStateAround - We have corrected the method isPositionValid

--- src/Grid.java
+++ src/Grid.java
@@ -14,13 +14,12 @@ public class Grid
 	/**
 	 * A two-dimensional matrix which contains lamp positions (x,y). Position (2,3) corresponds to lamps[2][3] 
 	 */
-	private Lamp[][] lamps;
-	private final int nbcolonnes=4;
-	private final int nblignes=4;
+	private boolean[][] stateOfLamps;
 	
-	private int i=0;
-	private int j=0;
 	
+	
+	private final static int NB_COLUMNS=4;
+	private final static int NB_LINES=4;
 	
 	/**
 	 * Creates a new 2D grid with randomly lit lamps
@@ -28,15 +27,20 @@ public class Grid
 	 */
 	public Grid()
 	{
-		this.lamps = new Lamp[nblignes][nbcolonnes];
+		this.stateOfLamps = new boolean[NB_LINES][NB_COLUMNS];
 	
 	}
+	
+	
+	/**
+	 * isAtLeastOneLampLit is a method that return true if there is at list one lamp lit, else it returns false 
+	 */
 
 	public boolean isAtLeastOneLampLit() 
 	{
-		for (int i=0;i<5;i++)
-			for (int j=0;i<5;i++)
-				if (lamps[i][j].stateoflamp) return true;
+		for (int i=0;i<NB_LINES;i++)
+			for (int j=0;j<NB_COLUMNS;j++)
+				if (stateOfLamps[i][j]==true) return true;
 		return false;
 		
 		
@@ -49,12 +53,12 @@ public class Grid
 	public boolean isPositionValid(Position position) {
 		// TODO Auto-generated method stub
 		
-			if (position.x>nbcolonnes && position.x<0)
+			if (position.getX()>NB_COLUMNS && position.getX()<0)
 			{
 				System.out.println("position invalide");
 				return false;
 			}
-			if (position.y>nblignes && position.y<0)
+			if (position.getY()>NB_LINES && position.getY()<0)
 			{
 				System.out.println("position invalide");
 				return false;
@@ -63,13 +67,23 @@ public class Grid
 			return true;	
 			
 	}	
+	
+	/**
+	 * switchStateAround is a method that changes the state of the four lamps around a position
+	 */
 
-	public void switchStateAround(Position position) {
-		// TODO Auto-generated method stub
-		
+	public void switchStateAround(Position position)
+	{
+		if(isPositionValid(position.getX()-1, position.getY())==true)
+		{
+		stateOfLamps[(position.getX())-1][position.getY()] = ! stateOfLamps[position.getX()-1][position.getY()];
+		}
+		stateOfLamps[(position.getX())+1][position.getY()] = ! stateOfLamps[position.getX()+1][position.getY()];
+		stateOfLamps[(position.getX())][position.getY()-1] = ! stateOfLamps[position.getX()][position.getY()-1];
+		stateOfLamps[(position.getX())][position.getY()+1] = ! stateOfLamps[position.getX()][position.getY()+1];
 	}
 
-	
+
 	
 	
 }
--- src/Position.java
+++ src/Position.java
@@ -1,12 +1,25 @@
 public class Position {
 
-	public  int x, y;
+	private  int x, y;
 	
 	public Position(int x0, int y0)
 	
 	{
 		this.x=x0;
 		this.y=y0;
+	}
+	
+	
+	public int getX()
+	{
+		return this.x;	
+		
+	}
+	
+	public int getY()
+	{
+		return this.y;	
+		
 	}
 	
 }
